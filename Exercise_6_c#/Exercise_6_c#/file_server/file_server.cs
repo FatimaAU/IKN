5fb90e6dcc84343114ed626815cd83736e238c93
FatimaAU <fatima_kodro@hotmail.com>
2018-02-28 19:52:38 +0100

    Added exercise template

---
+++ Exercise_6_c#/Exercise_6_c#/file_server/file_server.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcp
+{
+	class file_server
+	{
+		/// <summary>
+		/// The PORT
+		/// </summary>
+		const int PORT = 9000;
+		/// <summary>
+		/// The BUFSIZE
+		/// </summary>
+		const int BUFSIZE = 1000;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="file_server"/> class.
+		/// Opretter en socket.
+		/// Venter på en connect fra en klient.
+		/// Modtager filnavn
+		/// Finder filstørrelsen
+		/// Kalder metoden sendFile
+		/// Lukker socketen og programmet
+ 		/// </summary>
+		private file_server ()
+		{
+			// TO DO Your own code
+		}
+
+		/// <summary>
+		/// Sends the file.
+		/// </summary>
+		/// <param name='fileName'>
+		/// The filename.
+		/// </param>
+		/// <param name='fileSize'>
+		/// The filesize.
+		/// </param>
+		/// <param name='io'>
+		/// Network stream for writing to the client.
+		/// </param>
+		private void sendFile (String fileName, long fileSize, NetworkStream io)
+		{
+			// TO DO Your own code
+		}
+
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name='args'>
+		/// The command-line arguments.
+		/// </param>
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Server starts...");
+			new file_server();
+		}
+	}
+}
