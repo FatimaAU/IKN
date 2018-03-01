a19d7dc74062f36e05c5e66465b1e0c07e20f3ef
Fatima <au553932@uni.au.dk>
2018-03-01 21:14:28 +0100

    Server made iterative

--- Exercise_6_c#/server/file_server/file_server.cs
+++ Exercise_6_c#/server/file_server/file_server.cs
@@ -31,41 +31,45 @@ namespace tcp
 		/// </summary>
 		private file_server ()
 		{
+			string path = "/root/Documents/IKN/Exercise_6_c#/";
+
+
 			// Starts server and accepts a client
+			Console.WriteLine("Listening for client ..");
+
 			TcpListener serverSocket = new TcpListener(IPAddress.Any,PORT);
 			TcpClient clientSocket = default(TcpClient);
 			serverSocket.Start();
 			clientSocket = serverSocket.AcceptTcpClient();
+
 			Console.WriteLine("Accepted connection from client");
 
 			// Get the network file stream
 			NetworkStream clientStream = clientSocket.GetStream();
 
-
 			string filename = LIB.readTextTCP (clientStream);
-			Console.WriteLine ("Filename: " + filename);
-
-			string path = "/root/Documents/IKN/Exercise_6_c#/";
-
 			long fileSize = LIB.check_File_Exists (path + filename);
 
-			if (fileSize == 0)
+			while (LIB.check_File_Exists (path + filename) == 0)
 			{
 				string errorMsg = "File '" + filename + "' not found at '" + path + "'";
 				Console.WriteLine(errorMsg);
+
 				LIB.writeTextTCP (clientStream, fileSize.ToString());
-				return;
-				//string newFilename = LIB.readTextTCP (clientStream);
+
+				filename = LIB.readTextTCP (clientStream);
+				fileSize = LIB.check_File_Exists (path + filename);
+
 			} 
 
+			Console.WriteLine ("Filename: " + filename);
 			Console.WriteLine("Size: " + fileSize);
 			LIB.writeTextTCP (clientStream, fileSize.ToString());
 
 			sendFile (filename,  path, fileSize, clientStream);
 
-			//clientStream.Close ();
-
-
+			clientSocket.Close();
+			serverSocket.Stop();
 		}
 
 		/// <summary>
@@ -110,38 +114,10 @@ namespace tcp
 		static void Main(string[] args)
 		{
 			Console.WriteLine ("Server starts...");
-
 			while (true) 
 			{
 				new file_server ();
 			}
-
-//				try
-//				{
-//					NetworkStream networkStream = clientSocket.GetStream();
-//					byte[] bytesFrom = new byte[10025];
-//					networkStream.Read(bytesFrom, 0, bytesFrom.Length);
-//					string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
-//					dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("0"));
-//					//dataFromClient = dataFromClient.Substring(0, 1);
-//					Console.WriteLine(" >> Data from client - " + dataFromClient);
-//
-//					string serverResponse = "Last Message from client" + dataFromClient;
-//					Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
-//					networkStream.Write(sendBytes, 0, sendBytes.Length);
-//					networkStream.Flush();
-//					Console.WriteLine(" >> " + serverResponse);
-//				}
-//				catch (Exception ex)
-//				{
-//					Console.WriteLine(ex.ToString());
-//				}
-//
-//
-//			clientSocket.Close();
-//			serverSocket.Stop();
-			Console.WriteLine("Exit");
-			Console.ReadLine();
 		}
 
 	}
