66d76b5aedf1bb3ed11da59807ecd8a008fd6040
Fatima <au553932@uni.au.dk>
2018-03-15 09:42:34 +0100

    Added server code

--- Exercise_7_c#/UDPServer/UDPServer/Program.cs
+++ Exercise_7_c#/UDPServer/UDPServer/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
-namespace UDPServer
+namespace udp
 {
-	class MainClass
+	class UDPServer
 	{
-		public static void Main (string[] args)
+		const int PORT = 9000;
+
+	
+		private UDPServer()
 		{
-			Console.WriteLine ("Hello World!");
+			var server = new UdpClient (PORT);
+
+			while (true) 
+			{
+				IPEndPoint ep = new IPEndPoint(IPAddress.Any, PORT);
+				var data = server.Receive (ref ep); //listen on port 9000
+				Console.Write("receive data from: " + ep.ToString());
+
+				byte[] dataToSend = new byte[1] { 1 };
+				//dataToSend = ;
+				server.Send(dataToSend, dataToSend.Length, ep); //reply back
+			}
+		
+		}
+	
+		static void Main (string[] args)
+		{
+			Console.WriteLine ("Server starts...");
+			new UDPServer ();
 		}
 	}
 }
