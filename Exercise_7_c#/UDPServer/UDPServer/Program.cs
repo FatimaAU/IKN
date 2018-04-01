7af75484ecbd25edb6507e2b3e8682eaad5d9c78
Fatima <au553932@uni.au.dk>
2018-04-01 19:07:48 +0200

    Server working

--- Exercise_7_c#/UDPServer/UDPServer/Program.cs
+++ Exercise_7_c#/UDPServer/UDPServer/Program.cs
@@ -15,47 +15,55 @@ namespace udp
 	
 		private UDPServer()
 		{
+
 			var server = new UdpClient (PORT);
+
+			// Establish end point
+			IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, PORT);
 
 			while (true) 
 			{
-				IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, PORT);
-				byte[] receivedData = server.Receive (ref endPoint); //listen on port 9000
 
+				// Receive client input
+				byte[] receivedData = server.Receive (ref endPoint); //listen on port 9000
 				string data = Encoding.ASCII.GetString (receivedData);
 
+				Console.WriteLine ("Received client input: " + data);
 
-				GetMeasurement (data);
-				Console.WriteLine (data);
-				byte[] sentdataback = Encoding.ASCII.GetBytes (data);
-				server.Send (sentdataback, sentdataback.Length);					
+				// Get the requested info
+				string measure = GetMeasurement (data);
+
+				// Send data back
+				byte[] sentdataback = Encoding.ASCII.GetBytes (measure);
+				server.Send (sentdataback, sentdataback.Length, endPoint);					
 
 
-				Console.WriteLine("receive data from: " + data);
-
-//				byte[] dataToSend = new byte[1] { 1 };
-//				//dataToSend = ;
-//				server.Send(dataToSend, dataToSend.Length, ep); //reply back
 			}
 		
 		}
 
 		public string GetMeasurement(string letter)
 		{
-//			string uptime = System.IO.File.ReadAllText ("root/proc/uptime");
-//			string loadavg = System.IO.File.ReadAllText ("/~/proc/loadavg");
+			
+			string filePath;
 
-			if (letter == "u") 
+			switch (letter = letter.ToUpper()) 
 			{
-				return "derp";
-			}
-			if (letter == "U")
-			{
-				return "herp";
-			}
-			else
-				return "not the right letter";
+				case "U":
+					filePath = "/proc/uptime";
+					Console.WriteLine ("Sending uptime");
+					break;
+				case "L":
+					filePath = "/proc/loadavg";
+					Console.WriteLine ("Sending loadavg");
+					break;
+				default:
+					Console.WriteLine ("Bad input. Sending error message");
+					return "Bad input. Valid inputs are l (L) or u (U)\n";
+			
 
+			}
+			return "Reading from " + filePath + ": " + File.ReadAllText (filePath);
 		}
 	
 		static void Main (string[] args)
